537402202eda0c8ac6c078d3ff2803d0cd097f51
anura1402 <ann99909@gmail.com>
2024-06-20 23:52:13 +0300

    Initial commit

---
+++ app/src/main/java/ru/anura/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.anura.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
