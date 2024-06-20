3e48e62d9537f912ec88c1bc2bc7bce40af35363
anura1402 <ann99909@gmail.com>
2024-06-21 00:20:13 +0300

    Simple service with timer

--- app/src/main/java/ru/anura/myapplication/MainActivity.kt
+++ app/src/main/java/ru/anura/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.anura.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
---
+++ app/src/main/java/ru/anura/myapplication/MyService.kt
@@ -0,0 +1,47 @@
+package ru.anura.myapplication
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyService: Service() {
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("SERVICE_TAG","onCreate")
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        coroutineScope.launch {
+            for (i in 0 until 100){
+                delay(1000)
+                Log.d("SERVICE_TAG","Timer $i")
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        Log.d("SERVICE_TAG","onDestroy")
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+    companion object{
+        fun newIntent(context: Context):Intent{
+            return Intent(context,MyService::class.java)
+        }
+    }
+}
